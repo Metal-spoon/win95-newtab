98a6ca676c58c415ef29f1c373726a43be8eedac
Metal spoon <9281807+Metal-spoon@users.noreply.github.com>
2022-07-12 21:06:30 +0200

    Made topsites browser agnostic, removed history option Fixed favicons for both browsers

--- src/topsites.js
+++ src/topsites.js
@@ -1,49 +1,44 @@
+/*
+ *TODO:
+ *Template based DOM elements instead of hacky inline html appends
+ */
+
 export function initializeTopSites () {
   $('.topSiteList').empty()
-  const useHistory = JSON.parse(localStorage.getItem('usehistory'))
-  if (useHistory) {
-    chrome.history.search(
-      { text: '', startTime: 0, maxResults: 0 },
-      function (sites) {
-        const orderedSites = sites
-          .sort(function (a, b) {
-            return b.visitCount - a.visitCount
-          })
-          .slice(0, 10)
-        orderedSites.forEach((topSite) => {
-          $('.topSiteList').append(
-            "<a class=topSite href='" +
-              topSite.url +
-              "'><li class='topsiteContent'><img width=32px height=32px src=chrome://favicon/size/32/" +
-              topSite.url +
-              '/></br>' +
-              '<span>' +
-              topSite.title +
-              '</span>' +
-              '</li></a>'
-          )
-        })
-      }
-    )
+  // chrome.topSites.get.length = 1 = Firefox
+  // chrome.topSites.get.lenght = 0 = Chromium
+
+  if (chrome.topSites.get.length === 0) {
+    chrome.topSites.get((result) => {
+      buildTopsiteList(result)
+    })
   } else {
-    chrome.topSites.get(function (sites) {
-      sites.slice(0, 10).forEach((topSite, index) => {
-        console.log(index)
-        $('.topSiteList').append(
-          "<a class=topSite href='" +
-            topSite.url +
-            "'><li class=topsiteContent>" +
-            '<img id=test' +
-            index +
-            ' width=32px height=32px src=' +
-            '/></br>' +
-            '<span>' +
-            topSite.title +
-            '</span>' +
-            '</li></a>'
-        )
-        $('#test' + index).prop('src', topSite.favicon)
-      })
+    chrome.topSites.get({ includeFavicon: true }, (result) => {
+      buildTopsiteList(result)
     })
   }
+}
+
+function buildTopsiteList (sites) {
+  $('.topSiteList').empty()
+  sites.slice(0, 10).forEach((topSite, index) => {
+    $('.topSiteList').append(
+      "<a class=topSite href='" +
+        topSite.url +
+        "'><li class=topsiteContent>" +
+        '<img id=test' +
+        index +
+        ' width=32px height=32px src=' +
+        '/></br>' +
+        '<span>' +
+        topSite.title +
+        '</span>' +
+        '</li></a>'
+    )
+    if (topSite.favicon) {
+      $('#test' + index).prop('src', topSite.favicon)
+    } else {
+      $('#test' + index).prop('src', 'chrome://favicon/size/32/' + topSite.url)
+    }
+  })
 }
