7be1add856dd616dd3888444dd8f45b9e3bef3d5
Metal spoon <9281807+Metal-spoon@users.noreply.github.com>
2022-07-15 01:22:20 +0200

    removed custom flag in searchengine model

--- src/models/searchengine.js
+++ src/models/searchengine.js
@@ -2,13 +2,11 @@ export class SearchEngine {
   id
   name
   url
-  isCustom
 
-  constructor (id, name, url, isCustom) {
+  constructor (id, name, url) {
     this.id =
       id != null ? id : JSON.parse(localStorage.getItem('searchengines')).length
     this.name = name
     this.url = url
-    this.isCustom = isCustom
   }
 }
--- src/searchengines.js
+++ src/searchengines.js
@@ -4,12 +4,12 @@ export function initializeSearchEngines () {
   let searchEngines = JSON.parse(localStorage.getItem('searchengines'))
   if (!searchEngines) {
     searchEngines = [
-      new SearchEngine(0, 'Google', 'http://www.google.com/search?q=', false),
-      new SearchEngine(1, 'Yahoo', 'https://search.yahoo.com/search?p=', false),
-      new SearchEngine(2, 'Bing', 'https://www.bing.com/search?q=', false),
-      new SearchEngine(3, 'DuckDuckGo', 'https://duckduckgo.com/?q=', false),
-      new SearchEngine(4, 'Yandex', 'https://yandex.com/search/?text=', false),
-      new SearchEngine(5, 'Ecosia', 'https://www.ecosia.org/search?q=', false)
+      new SearchEngine(0, 'Google', 'http://www.google.com/search?q='),
+      new SearchEngine(1, 'Yahoo', 'https://search.yahoo.com/search?p='),
+      new SearchEngine(2, 'Bing', 'https://www.bing.com/search?q='),
+      new SearchEngine(3, 'DuckDuckGo', 'https://duckduckgo.com/?q='),
+      new SearchEngine(4, 'Yandex', 'https://yandex.com/search/?text='),
+      new SearchEngine(5, 'Ecosia', 'https://www.ecosia.org/search?q=')
     ]
     localStorage.setItem('searchengines', JSON.stringify(searchEngines))
   }
