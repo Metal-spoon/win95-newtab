3f5d07b5253e5d3f903c7cedfbce79258b6d832f
Metal spoon <9281807+Metal-spoon@users.noreply.github.com>
2022-09-20 14:55:54 +0200

    added startpage search engine

--- src/js/settings/defaults.js
+++ src/js/settings/defaults.js
@@ -12,7 +12,8 @@ export const SearchEngineList = [
   new SearchEngine(2, 'Bing', 'https://www.bing.com/search?q='),
   new SearchEngine(3, 'DuckDuckGo', 'https://duckduckgo.com/?q='),
   new SearchEngine(4, 'Yandex', 'https://yandex.com/search/?text='),
-  new SearchEngine(5, 'Ecosia', 'https://www.ecosia.org/search?q=')
+  new SearchEngine(5, 'Ecosia', 'https://www.ecosia.org/search?q='),
+  new SearchEngine(6, 'Startpage', 'https://www.startpage.com/sp/search?query=')
 ]
 
 const SelectedSearchEngine = SearchEngineList[0]
